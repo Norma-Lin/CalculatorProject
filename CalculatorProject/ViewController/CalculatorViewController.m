d3af04ab2ec50420e6efab08b4a14b3df5927a0a
Norma-Lin <girl1112ut@gmail.com>
2021-10-02 22:33:31 +0800

    新增property和完成clearButton、deleteButton、pointButton功能

--- CalculatorProject/ViewController/CalculatorViewController.m
+++ CalculatorProject/ViewController/CalculatorViewController.m
@@ -8,10 +8,31 @@
 
 #import "CalculatorViewController.h"
 
+
+typedef NS_ENUM(NSInteger, OperationType) {
+    OperationTypePlus = 0,
+    OperationTypeMinus = 1,
+    OperationTypeMultiply = 2,
+    OperationTypeDivision = 3,
+    OperationTypeNone = 4
+};
+
 @interface CalculatorViewController ()
 
-@property (weak, nonatomic) IBOutlet UITextField *m_tfDisplay;
-@property (weak, nonatomic) IBOutlet UIButton *m_btnAC;
+@property (weak, nonatomic) IBOutlet UITextField *calculationView;
+
+//紀錄運算符號
+@property (strong, nonatomic) NSString *calculationSymbol;
+//記錄目前數字
+@property (assign, nonatomic) double nowNumber;
+//記錄上一個數字
+@property (assign, nonatomic) double previousNunber;
+//紀錄是否運算中
+@property (assign, nonatomic) BOOL isCalculation;
+//紀錄是否為新運算
+@property (assign, nonatomic) BOOL isNew;
+//記錄目前運算
+@property (assign, nonatomic) OperationType operation;
 
 @end
 
@@ -20,22 +41,85 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.isNew = YES;
+    self.operation = OperationTypeNone;
+}
+
+- (void)setCalculationSymbol:(NSString *)calculationSymbol {
     
-    [self addBtnTarget];
+    _calculationSymbol = calculationSymbol;
+    NSLog(@"calculationSymbol = %@", _calculationSymbol);
 }
 
-- (void)addBtnTarget
-{
-    [self.m_btnAC addTarget:self
-                     action:@selector(onClickAC:)
-           forControlEvents:UIControlEventTouchUpInside];
+- (void)setNowNumber:(double)nowNumber {
+    
+    _nowNumber = nowNumber;
+    NSLog(@"nowNumber = %f", _nowNumber);
 }
 
-- (void)onClickAC:(id)sender
-{
-    // Reset to Default
-    // ...
-    self.m_tfDisplay.text = @"";
+- (void)setPreviousNunber:(double)previousNunber {
+    
+    _previousNunber = previousNunber;
+    NSLog(@"previousNunber = %f", _previousNunber);
 }
+
+- (IBAction)clearButton:(UIButton *)sender {
+    
+    self.calculationView.text = @"0";
+    self.calculationSymbol = @"";
+    self.nowNumber = 0;
+    self.previousNunber = 0;
+    self.isCalculation = NO;
+    self.isNew = YES;
+}
+
+- (IBAction)plusMinusChangeButton:(UIButton *)sender {
+}
+
+- (IBAction)factorialButton:(UIButton *)sender {
+}
+
+- (IBAction)deleteButton:(UIButton *)sender {
+    
+    if ([self.calculationView.text isEqualToString:@""]) {
+        return;
+    }
+    
+    NSString *number = self.calculationView.text;
+    NSLog(@"原始text = %@", number);
+    
+    if (number.length == 1) {
+        
+        self.calculationView.text = @"0";
+        self.isCalculation = NO;
+        self.isNew = YES;
+        self.nowNumber = 0;
+    }
+    else if ([number containsString:@"e"]) {
+        
+        self.calculationView.text = @"0";
+        self.isCalculation = NO;
+        self.isNew = YES;
+        self.nowNumber = 0;
+    }
+    else {
+        
+        NSString *deleteNumber = [number substringToIndex:[number length]-1];
+        self.calculationView.text = deleteNumber;
+        self.nowNumber = [deleteNumber doubleValue];
+    }
+    
+}
+
+- (IBAction)pointButton:(UIButton *)sender {
+    
+    NSString *number = self.calculationView.text;
+    if ([number containsString:@"."] || [self.calculationView.text isEqualToString:@""]) {
+        return;
+    }
+    self.calculationView.text = [NSString stringWithFormat:@"%@%@", number, @"."];
+    self.isNew = NO;
+}
+
 
 @end
